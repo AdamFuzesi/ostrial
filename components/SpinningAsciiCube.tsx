7b633a82811a53a5b7b5ec26d21022eefbb351f5
AdamFuzesi <adamfuzeri@gmail.com>
2025-12-08 01:34:56 +0900

    adding spacing

--- components/SpinningAsciiCube.tsx
+++ components/SpinningAsciiCube.tsx
@@ -50,6 +50,7 @@ export const SpinningAsciiCube = ({ color = "white" }: SpinningAsciiCubeProps) =
   }
 
   const calculateForSurface = (
+    
     cubeX: number,
     cubeY: number,
     cubeZ: number,
